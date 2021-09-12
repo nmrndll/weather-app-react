0c0d9648a9094024ba9afb709090fb4b2aef9593
nmrndll <81620556+nmrndll@users.noreply.github.com>
2021-09-12 16:13:25 +0800

    Set up in Github

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles.css';
 import reportWebVitals from './reportWebVitals';
+import Search from './Search';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
+
+
+    </div>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
