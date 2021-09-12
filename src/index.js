936d38595a4a716b1f7cb57bba1b8ed4838ff742
nmrndll <81620556+nmrndll@users.noreply.github.com>
2021-09-12 16:30:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import WeatherSearch from "./WeatherSearch";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
-import reportWebVitals from './reportWebVitals';
-import Search from './Search';
+import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
+function App() {
+  return (
     <div className="App">
       <h1>Weather App</h1>
-      <Search />
-
-
+      <WeatherSearch />
     </div>
-  
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
